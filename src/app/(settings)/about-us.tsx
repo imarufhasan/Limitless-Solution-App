4fbf7d11bc4bf9c76a6cabb955bb94d1f31e9146
shukumar-542 <shukumar542@gmail.com>
2026-06-16 15:04:14 +0600

    Implement terms and conditions and privacy and policy

--- src/app/(settings)/about-us.tsx
+++ src/app/(settings)/about-us.tsx
@@ -1,27 +1,39 @@
 import BackButton from '@/components/shared/BackButton'
+import { useContentManagementQuery } from '@/redux/features/auth/authApi'
 import React from 'react'
-import { ScrollView, Text, View } from 'react-native'
+import { ActivityIndicator, ScrollView, useWindowDimensions, View } from 'react-native'
+import RenderHTML from 'react-native-render-html'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-export default function AboutUs
-  () {
+export default function AboutUs() {
+  const { data, isLoading } = useContentManagementQuery("about_us")
+  const { width } = useWindowDimensions();
+
+  if (isLoading) {
+    return (
+      <SafeAreaView
+        style={{ flex: 1, backgroundColor: '#F8F6FA' }}
+        edges={['top']}
+      >
+        <View className="flex-1 justify-center items-center">
+          <ActivityIndicator size="large" color="#652D8B" />
+        </View>
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#F8F6FA' }} edges={['top']}>
       <View className="flex-1 px-4">
         <ScrollView showsVerticalScrollIndicator={false}>
           <BackButton title='About Us' textColor='#652D8B' showBorder />
-          <View className="bg-[#F0EAF3] rounded-md shadow-xl p-4 mt-4 flex-1">
-            <Text style={{ fontFamily: "Inter_400Regular" }} className="text-sm text-[#0F0B18] leading-6">
-              gravida elit enim. lobortis, ex orci lobortis, Donec orci elit felis, luctus ultrices odio tincidunt cursus elit ex nisi vehicula, Morbi Nunc Morbi venenatis sollicitudin. tortor. dui non quam dui. nibh tortor. sit viverra maximus ipsum
-
-              massa tincidunt massa non, Ut ex lobortis, nulla, sit orci Nam massa viverra venenatis massa placerat In viverra laoreet massa Lorem at elit scelerisque Quisque viverra id ipsum risus quam Lorem id quis ultrices vel placerat dui. elit nec
-
-              lobortis, vehicula, tempor Quisque sed felis, vitae Sed varius dolor volutpat in sed non, massa sit porta nisi ex. porta nulla, turpis efficitur. Nunc dolor dolor id non est. lacus, varius ipsum placerat. elementum dignissim, Vestibulum
-
-              quam efficitur. gravida non. lacus, vehicula, nec id commodo turpis Donec Nam faucibus quis elementum tincidunt tortor. orci adipiscing odio sed sollicitudin. eget quis faucibus diam Cras fringilla Nam Lorem adipiscing vel in Vestibulum
-
-
-            </Text>
+          <View className="bg-[#F0EAF3] rounded-md shadow-xl p-4 mt-4 flex-1 mb-10">
+            <RenderHTML
+              contentWidth={width}
+              source={{
+                html: data?.data?.content || '',
+              }}
+            />
           </View>
         </ScrollView>
       </View>
--- src/app/(settings)/privacyPolicy.tsx
+++ src/app/(settings)/privacyPolicy.tsx
@@ -5,10 +5,11 @@ import { ActivityIndicator, ScrollView, useWindowDimensions, View } from 'react-
 import RenderHtml from 'react-native-render-html'
 import { SafeAreaView } from 'react-native-safe-area-context'
 export default function PrivacyPolicy() {
+
   const { data, isLoading } = useContentManagementQuery("privacy_policy")
   const { width } = useWindowDimensions();
 
-  // console.log("Privacy Pilicy",data?.data?.content)
+  // 
    if (isLoading) {
     return (
       <SafeAreaView
@@ -21,12 +22,13 @@ export default function PrivacyPolicy() {
       </SafeAreaView>
     );
   }
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#F8F6FA' }} edges={['top']}>
       <View className="flex-1 px-4">
         <ScrollView showsVerticalScrollIndicator={false}>
           <BackButton title='Privacy Policy ' textColor='#652D8B' showBorder />
-          <View className="bg-[#F0EAF3] rounded-md shadow-xl p-4 mt-4 flex-1">
+          <View className="bg-[#F0EAF3] rounded-md shadow-xl p-4 mt-4 flex-1 mb-10">
             <RenderHtml
               contentWidth={width}
               source={{
